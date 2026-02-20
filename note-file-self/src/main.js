9127fcc5332896d9ab246211e115834d3426e8d9
Randy <imagine90359@yahoo.com.tw>
2026-02-20 16:34:23 +0800

    'fix'

--- note-file-self/src/main.js
+++ note-file-self/src/main.js
@@ -3,10 +3,10 @@ import App from './App.js';
 import router from './router.js';
 import { initDataBase, getAllNotes, createNote, getNote } from './services/database.js';
 
-console.log(await initDataBase())
+// console.log(await initDataBase())
 // await createNote("My First Note", "This is the content of my first note.", 0)
-console.log(getAllNotes())
-console.log(getNote(1))
+// console.log(getAllNotes())
+// console.log(getNote(1))
 
 const app = createApp(App)
 app.use(router);
